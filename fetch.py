739d232e8a7dd91c62977022a9f676c9d108a187
adamyty <s892351@gmail.com>
2018-07-25 01:04:52 +0800

    Complete fetch function To do : error handling when fetch error

--- fetch.py
+++ fetch.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import sqlite3
+import time
 import twstock
 from collections import namedtuple, defaultdict
 from datetime import date, datetime
@@ -27,32 +28,52 @@ def connect(stockid):
 
 		db.commit()
 	else:
-		print(dbpath + " does exists")
+		print(dbpath + " exists")
 	return db
 
 
 
 stockid = 2330
-# set period
-fetch_start_year = 2018
-fetch_start_month = 7
-fetch_period = [(x, y)	for x in range(int(fetch_start_year), int(date.today().year) + 1) \
-						for y in range(int(fetch_start_month), int(date.today().month) + 1) \
-						if (x, y) <= (int(date.today().year), int(date.today().month))  ]
+
 # open db
 db = None
 db = connect(stockid)
+
+# set fetch start date and last update date
+'''
+fetch last record in db to fetch non-sync data
+date format : (str) 2018-07-23
+'''
+cursor = db.cursor()
+cursor.execute("SELECT date FROM stockdata LIMIT 1")
+#if new db created
+if cursor.fetchone() is None:
+	last_record_date = "2000-01-01"
+	fetch_start_year = 2000
+	fetch_start_month = 1
+else:
+	cursor.execute("SELECT * FROM stockdata ORDER BY date DESC")
+	last_record_date = cursor.fetchone()[0]
+	fetch_start_year, _, _ = last_record_date.split('-')
+	fetch_start_year = int(fetch_start_year)
+	fetch_start_month = 1
+
+print("""
+last record date : %r
+fetch start year : %r
+fetch start month : %r
+	""" %(last_record_date, fetch_start_year, fetch_start_month))
+# set period
+#fetch_start_year = 2018
+#fetch_start_month = 7
+fetch_period = [(x, y)	for x in range(int(fetch_start_year), int(date.today().year) + 1) \
+						for y in range(1, 13) \
+						if (x, y) <= (int(date.today().year), int(date.today().month))  ]
 
 # fetch stock data
 STOCK_DATA = namedtuple("STOCK_DATA", "date capacity turnover open high low close change tx")
 stock = twstock.Stock(str(stockid))
 cursor = db.cursor()
-'''
-fetch last record in db to fetch non-sync data
-date format : (str) 2018-07-23
-'''
-cursor.execute("SELECT * FROM stockdata ORDER BY date DESC")
-last_record_date = cursor.fetchone()[0]
 
 for i in fetch_period:
 	print(i)
@@ -62,6 +83,7 @@ for i in fetch_period:
 		print("There are %r datas for %r" %(len(stock_datas), stockid))
 	else:
 		print("There is no newer data (last record date : %r)" %(last_record_date))
+		continue
 	for j in stock_datas:
 		print(*j)
 #        print("{0.date}\t".format(j))
@@ -71,9 +93,14 @@ for i in fetch_period:
 		cursor.execute("INSERT INTO stockdata (date, capacity, turnover, open, high, low, close, change, tx) VALUES ('{0.date}', {0.capacity}, {0.turnover}, {0.open}, {0.high}, {0.low}, {0.close}, {0.change}, {0.tx})".format(j))
 		db.commit()
 
+	print(	"""
+			===========================
+			""")
+	time.sleep(10)
+
 #close db
 if db is not None:
-	print("db is not None")
+	print("db is not None, close it")
 	db.close
 
 
